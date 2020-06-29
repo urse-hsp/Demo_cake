4235f0c1840082fac36fb66a5157ca1e01951b1b
侯少平 <hsp_email@163.com>
2020-06-29 15:17:21 +0800

    update src/App.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import AppRouter from './AppRouter';
 import {Provider} from 'react-redux';
+// 可以单独拉入文件夹
 import {createStore} from 'redux';
 import reducer from './reducer';
 let store = createStore(reducer);
